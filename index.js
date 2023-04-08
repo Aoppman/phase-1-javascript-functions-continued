b7fc65ca2ec97937b1db649518caa6bc5eda7ccd
Andrew <a.oppman@gmail.com>
2023-04-08 01:50:09 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// code your solution here
+
+//Define a function using FUNCTION DECLARATION
+saturdayFun()
+//function
+function saturdayFun(activity) {
+
+    if (activity === undefined) {
+        return "This Saturday, I want to roller-skate!";
+    }
+    else {
+        return `This Saturday, I want to ${activity}!`;
+    }
+};
+
+
+//Define a function using a FUNCTION EXPRESSION
+
+//Funtion
+const mondayWork = function whatToDo(activity) {
+        if (activity === undefined) {
+            return "This Monday, I will go to the office.";
+        }
+        else {
+            return `This Monday, I will ${activity}.`
+        };
+};
+
+//Functtion expression
+ mondayWork()
+
+
+function wrapAdjective(special) {
+   
+    const result = function (emphatic) {
+    if (special === '*') {
+        return `You are *${emphatic}*!`;
+    }
+    else {
+        return `You are ||${emphatic}||!`;
+    }
+    };
+
+ return result;
+ };
+
+  
